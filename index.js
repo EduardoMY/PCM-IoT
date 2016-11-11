353ed168aee1532be03f0e805e82a4430306f61a
eduardomy <eduardo.medina@cetys.edu.mx>
2016-11-11 13:54:44 -0800

    maybe there was a problem with fors

--- index.js
+++ index.js
@@ -1,27 +1,3 @@
-
-/*
-* Copyright (c) 2015 - 2016 Intel Corporation.
-*
-* Permission is hereby granted, free of charge, to any person obtaining
-* a copy of this software and associated documentation files (the
-* "Software"), to deal in the Software without restriction, including
-* without limitation the rights to use, copy, modify, merge, publish,
-* distribute, sublicense, and/or sell copies of the Software, and to
-* permit persons to whom the Software is furnished to do so, subject to
-* the following conditions:
-*
-* The above copyright notice and this permission notice shall be
-* included in all copies or substantial portions of the Software.
-*
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-* NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-* LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-* OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-* WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 "use strict";
 
 //Something Useful
@@ -84,12 +60,11 @@ function server() {
 	console.log(req.params);
 	console.log(req.body.paths.length);
 	console.log("Inicio for");
-	for (var p in req.body.paths){
-	    console.log(p.length);
-	    console.log(p);
-	    for(var point in p){
+	for (var i=0;  i<req.body.paths.length; i++){
+	    console.log("un trazo");
+	    for(var ic=0; ic<req.body.paths[i].length ;ic ){
 		console.log("un punto");
-		console.log("x "+point.pX + "y"+point.pY);
+		console.log("x "+req.body.paths[i][ic].pX + "y"+req.body.paths[i][ic].pY);
 	    }
 	}
 	console.log("fin for");
