25fd2d92c76f0d24cef0442a2dd88a851790dfe6
eduardomy <eduardo.medina@cetys.edu.mx>
2016-11-11 08:46:33 -0800

    Some changes

--- index.js
+++ index.js
@@ -31,10 +31,6 @@ var paso1 = Number(8);   //pin para mandar pasos eje x
 var dir2 = Number(7);   ////pin para direccion eje y
 var paso2 = Number(6);   //pin para mandar pasos eje y
 
-
-var CLOCKWISE = 1,
-    COUNTERCLOCKWISE = 2;
-
 // The program is using the Node.js built-in `fs` module
 // to load the html file used to view the range finder status
 var fs = require("fs");
@@ -43,30 +39,20 @@ var fs = require("fs");
 // the file path to the html file used to view the range finder status
 var path = require("path");
 
-// Initialize the hardware devices
-var ULN200XA = require("jsupm_uln200xa");
-var thumb = new (require("jsupm_joystick12").Joystick12)(0, 1),
-    step1 = new ULN200XA.ULN200XA(4096, 9, 10, 11, 12),
-    step2 = new ULN200XA.ULN200XA(4096, 4, 5, 6, 7);
+var mraa = require('mraa'); //require mraa
+console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
 
-// Moves the stepper motor
-function move(stepper, dir, speed, steps) {
-  console.log("move", dir || CLOCKWISE, steps || 1020);
-  stepper.setSpeed(5);
-  stepper.setDirection(dir || CLOCKWISE);
-  stepper.stepperSteps(steps || 1020);
-}
+//
+var pinDir1 = new mraa.Gpio(9);
+var pinDir2 = new mraa.Gpio(7);
+var pinPaso1 = new mraa.Gpio(8);
+var pinPaso2 = new mraa.Gpio(6);
 
-// Helper function to scale to needed -1..1 range
-function scale(n) {
-  // convert down to 0..1
-  var val = (n - -0.5) / -0.4;
-  if (val > 1) { val = 1; }
-  if (val < 0) { val = 0; }
-
-  // and then to -1..1 and round
-  return Math.round(val * 2 - 1);
-}
+//Sets the direction of the Pins to Output, necessary for a digitalWrite
+pinDir1.dir(mraa.DIR_OUT);
+pinDir2.dir(mraa.DIR_OUT);
+pinPaso1.dir(mraa.DIR_OUT);
+pinPaso2.dir(mraa.DIR_OUT);
 
 // Starts the built-in web server for the web page
 // used to view or control the arm
@@ -82,16 +68,18 @@ function server() {
     fs.readFile(path.join(__dirname, "index.html"), {encoding: "utf-8"}, serve);
   }
 
-  app.get("/", index);
+    app.get("/", index);
+    
 
-  // Handler for each of the RESTful endpoints to control the arm
+    // Handler for each of the RESTful endpoints to control the arm
+    /*
   function handle(stepper, dir) {
     return function(req, res) {
       res.send("done");
       move(stepper, dir);
     };
   }
-
+    
   // motor 1
   app.post("/one-cw", handle(step1, CLOCKWISE));
   app.post("/one-ccw", handle(step1, COUNTERCLOCKWISE));
@@ -99,7 +87,10 @@ function server() {
   // motor 2
   app.post("/two-cw", handle(step2, CLOCKWISE));
   app.post("/two-ccw", handle(step2, COUNTERCLOCKWISE));
-
+    */
+    app.post("/move", function (req, res){
+	console.log(req.params);
+    });
   app.listen(process.env.PORT || 3000);
 }
 
@@ -113,31 +104,11 @@ function moveStep(pinDir, pinPas, dir){
 }
 
 function main() {
-    var mraa = require('mraa'); //require mraa
-    console.log('MRAA Version: ' + mraa.getVersion()); //write the mraa version to the console
-    var pinDir1 = new mraa.Gpio(9);
-    var pinDir2 = new mraa.Gpio(7);
-    var pinPaso1 = new mraa.Gpio(8);
-    var pinPaso2 = new mraa.Gpio(6);
-    pinDir1.dir(mraa.DIR_OUT); //set the gpio direction to output
-    pinDir2.dir(mraa.DIR_OUT);
-    pinPaso1.dir(mraa.DIR_OUT);
-    pinPaso2.dir(mraa.DIR_OUT);
     server();
     
     setInterval(function() {
-	moveStep(pinDir1, pinPaso1);
-	moveStep(pinDir2, pinPaso2);
-	/*
-//	digitalWrite(dir1,LOW);
-	pinDir1.write(0); //set the digital pin to high (1)
-//	digitalWrite(paso1,HIGH);
-	pinPaso1.write(1); //set the digital pin to high (1)
-	setTimeout(function(){
-//	    digitalWrite(paso1,LOW);
-	    pinPaso1.write(0);
-	    console.log('wii');
-	}, T/2); */
+	moveStep(pinDir1, pinPaso1, 0);
+	moveStep(pinDir2, pinPaso2, 0);
     }, T);
 }
 
