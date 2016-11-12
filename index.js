00e217905d7b6e6288ac0954d3a2bf7889cf852c
eduardomy <eduardo.medina@cetys.edu.mx>
2016-11-12 00:15:05 -0800

    CHange direction values

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ function server() {
     function startMoving(paths){
 //	console.log("should be doing");
 	    if(movementInX(paths)!=0){//checks if needs movement in x
-		moveStep(pinDir1, pinPaso1, (movementInX(paths)<0 ? 1 : 0));
+		moveStep(pinDir1, pinPaso1, (movementInX(paths)>0 ? 1 : 0));
 		
 		if(movementInX(paths)>0)
 		    currentX++;
@@ -111,7 +111,7 @@ function server() {
 		    currentX--;
 	    }
 	    else if(movementInY(paths)!=0){// checks if needs movement in Y
-		moveStep(pinDir2, pinPaso2, (movementInY(paths)<0 ? 1 : 0));
+		moveStep(pinDir2, pinPaso2, (movementInY(paths)>0 ? 1 : 0));
 		
 		if(movementInY(paths)>0)
 		    currentY++;
